1b81fdcd63eacb5bd635646b41ed5b64a24cce2d
Ravi Netravali <ravinet@mit.edu>
2013-07-02 20:07:52 -0400

    Fix MAC address assignment; read IPs from file; create separate apache server for each Mininet instance

--- web_mirror.py
+++ web_mirror.py
@@ -10,13 +10,21 @@ from mininet.node import Host
 from mininet.link import Link
 
 import os
-
+import sys
 #TODO: Read from file or use Python's source command
-ip_addr = ['']*4
-ip_addr[0] = '128.30.2.155'
-ip_addr[1] = '74.125.228.4'
-ip_addr[2] = '74.125.228.9'
-ip_addr[3] = '74.125.228.1'
+if (len(sys.argv) == 0):
+  ifile = 'serverips.txt'
+else:
+  c = str(sys.argv[1])
+  ifile = c + '/' + c + 'serverips.txt'
+ip_addr=[]
+for f in open(ifile):
+  ip_addr.append(f.strip())
+#ip_addr = ['']*4
+#ip_addr[0] = '128.30.2.155'
+#ip_addr[1] = '74.125.228.4'
+#ip_addr[2] = '74.125.228.9'
+#ip_addr[3] = '74.125.228.1'
 
 #################################
 class ProtoTester(Topo):
@@ -33,8 +41,12 @@ class ProtoTester(Topo):
 
         # IPs for sharded servers
         server_list = []
-        for i in range(0,4):
-          server_list.append(self.addHost('server'+str(i), ip=ip_addr[i], mac='00:00:00:00:00:0'+str(i+2)))
+        for i in range(0,len(ip_addr)):
+          if i < 8:
+            server_list.append(self.addHost('server'+str(i), ip=ip_addr[i], mac='00:00:00:00:00:0'+str(i+2)))
+          if i > 7:
+            server_list.append(self.addHost('server'+str(i), ip=ip_addr[i], mac='00:00:00:00:00:'+str(i+2)))
+
           # Add links
           self.addLink(s1, server_list[i])
 
@@ -43,7 +55,8 @@ if __name__ == '__main__':
     # Configure and start NATted connectivity
     os.system( "killall -q controller" )
     os.system( "killall -q phantomjs" )
-
+    os.system( "/etc/init.d/apache2 stop" )
+    os.system( "killall -s9 apache2" )
     topo = ProtoTester()
     net = Mininet(topo=topo, host=Host, link=Link)
     net.start()
@@ -54,7 +67,7 @@ if __name__ == '__main__':
     client.waitOutput()
     client.sendCmd('route add default dev client-eth0')
     client.waitOutput()
-
+   
     # Set /32 netmask and route for all servers
     server_names = ['']*len(ip_addr)
     for i in range(0,len(ip_addr)):
@@ -63,7 +76,13 @@ if __name__ == '__main__':
       server_names[i].waitOutput()
       server_names[i].sendCmd('route add default dev server'+str(i)+'-eth0')   
       server_names[i].waitOutput()
+      server_names[i].sendCmd('/home/ravinet/start_apache.sh ' + str(i))
+      server_names[i].waitOutput()
+
 
     print "*** Hosts are running and can talk to each other"
     print "*** Type 'exit' or control-D to shut down network"
     CLI( net )
+    os.system( "cp /etc/hosts_original /etc/hosts" )
+    for u in range(0, len(ip_addr)):
+      os.system( "rm /etc/apache2/apache2" + str(u) + ".conf")
