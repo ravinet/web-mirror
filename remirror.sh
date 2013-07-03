e8db3427650aed99e5aa229219e6bf7322389ce8
Ravi Netravali <ravinet@mit.edu>
2013-07-02 21:22:48 -0400

    Save some trouble by reusing previously mirrored site

---
+++ remirror.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+site=$1
+
+#run script to create file to add to /etc/hosts (ips.txt) and create directories according to url paths and fill them with wget objects
+python reset_mirror.py $site
+
+#run mininet
+python web_mirror.py $site
+
+
+ 
