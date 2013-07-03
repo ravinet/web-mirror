7f18a6740cf66f1f2a1f39cbe8b78072770ba5e7
Ravi Netravali <ravinet@mit.edu>
2013-07-02 20:50:16 -0400

    Commit wrapper shell script

---
+++ mirror.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+set -x
+set -v
+site=$1
+folder=$2
+rm -rf $folder
+rm -rf /var/www/$folder
+mkdir /var/www/$folder
+
+# Modify root folder
+cd /etc/apache2/sites-available
+sed s/"DocumentRoot \/var\/www"/"DocumentRoot \/var\/www\/$folder"/g default > default.new
+mv default default.backup
+mv default.new default
+cd -
+
+##run phantomjs get_info to create file for website with urls+ips
+/home/ravinet/testphantomJS/phantomjs/bin/phantomjs /home/ravinet/testphantomJS/phantomjs/examples/get_info.js $site > tempgets.txt
+#
+##run script to create file to add to /etc/hosts (ips.txt) and create directories according to url paths and fill them with wget objects
+python web_mirror_record.py tempgets.txt $folder
+#sleep 1
+#run mininet
+python web_mirror.py $folder
+
+# restore
+cd /etc/apache2/sites-available
+mv default.backup default
+cd -
