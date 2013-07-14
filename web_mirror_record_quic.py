dbaf9691614c1ca7566e5f3cdec79ccdc17508cc
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-07-14 18:25:43 -0400

    quic starts on mininet servers

---
+++ web_mirror_record_quic.py
@@ -0,0 +1,135 @@
+import sys
+import os
+import shutil
+#apac = open('etc/apache2/apache2.conf', 'a')
+# file name with gets and ips
+getinfo = sys.argv[1]
+print getinfo
+
+# root folder to save in
+mirror_root = sys.argv[2]
+
+# working directory
+working_dir = sys.argv[3]
+
+# keep track of distinct IPs in gets file for etc/hosts
+ips = []
+host_mapping = open('ips.txt', 'w')
+
+# File with list of IPs for Mininet setup
+mininet_cfg  = open('serverips.txt','w')
+
+# Number of distinct IPs
+count = 0
+
+#mirror_path is folder where wgets should be stored (outmost directory)   
+mirror_path = '/home/skype-alpha/July3rdGoogleMeeting/src/out/Release/' + str(mirror_root)
+
+# remove old vestiges
+os.system("rm -rf "+mirror_path);
+
+#get only first entries domain_name for folder
+linecount = 0
+quic_folder = ''
+
+# Parse gets.txt
+for line in open(getinfo):
+  records          = line.split(' ')
+  scheme_plus_url  = records[0]
+  ip = records[1]
+
+  domain_name     = ''
+  resource_folder = ''
+  if 'https' in scheme_plus_url:
+    domain_name      = scheme_plus_url.split('https://')[1].split('/')[0]
+    resource_folders = scheme_plus_url.split('https://')[1].split('/')
+  elif 'http' in scheme_plus_url:
+    domain_name      = scheme_plus_url.split('http://')[1].split('/')[0]
+    resource_folders = scheme_plus_url.split('http://')[1].split('/')
+  else :
+    print "Die here. Found non HTTP/HTTPS"
+    exit(5)
+  if linecount == 0:
+    quic_folder = domain_name
+  print "Domain name", domain_name
+  print "resource_folders", resource_folders
+  linecount = linecount + 1
+  if ip not in ips:
+    # Saw distinct IP
+    ips.append(ip)
+
+    # Add to host mapping file for DNS
+    host_mapping.write(str(ip.strip()) + ' ' + domain_name + '\n')
+
+    # Add to list of IPs for Mininet
+    mininet_cfg.write(str(ip.strip()) + '\n')
+
+    count = count + 1
+
+  #directories to be made for wget
+  resource_directory = mirror_path
+  for i in range(1, len(resource_folders)-1):
+    # start from 1 because 0 is hostname
+    # end at -1 because the last entry is the file itself
+    resource_directory = resource_directory + '/' + resource_folders[i]
+
+  print "resource_directory",resource_directory
+
+  # Create directory for resource if it doesn't exist
+  if not os.path.exists(resource_directory):
+    os.makedirs(resource_directory)
+
+  # Change directory to copy resource into it's final resting place
+  os.chdir(resource_directory)
+
+  # Get name of resource
+  resource_name = resource_folders[-1]
+
+  # Get rid of query parameters
+  resource_name = resource_name.split('?')[0]
+  print "resource_name",resource_name
+
+  # Reconstruct URL for fetching
+  url_records = scheme_plus_url.split('/')
+  
+  # Concatenate all except the last record
+  fetch_url = ''
+  for record in url_records[:-1] :
+    fetch_url += record+'/'
+  fetch_url += resource_name
+
+  print "fetch_url",fetch_url,"\n"
+
+  # Finally, fetch resource using wget
+  os.system("wget --save-headers \"" + str(fetch_url) + "\"")
+  #Get rid of Transfer-Encoding: chunked so quic_server can load files into cache
+  sed_cmd = " sed -i '/Transfer-Encoding: chunked/ d' " + resource_name
+  os.system(sed_cmd)
+  # Change directory back to original directory
+  os.chdir(working_dir)
+
+#make sure index.html gets rid of Transfer-Encoding: chunked
+index_path = mirror_path + '/index.html'
+sed_cmd = " sed -i '/Transfer-Encoding: chunked/ d' " + index_path
+os.system(sed_cmd)
+# Close Mininet Config 
+mininet_cfg.close()
+host_mapping.close()
+
+# Concatenate to /etc/hosts
+dns_mapping = open('/etc/hosts', 'w')
+dns_mapping.write('127.0.0.1       localhost\n128.30.76.203   copley.csail.mit.edu    copley\n 127.0.1.1 skypealpha-ThinkCentre-M91p \n')
+for entry in open('ips.txt'):
+  dns_mapping.write(entry)
+
+dns_mapping.write('\n\n# The following lines are desirable for IPv6 capable hosts\n::1     ip6-localhost ip6-loopback\nfe00::0 ip6-localnet\nff00::0 ip6-mcastprefix\nff02::1 ip6-allnodes\nff02::2 ip6-allrouters')
+dns_mapping.close()
+# Save mirror setup for posterity
+os.system("rm -rf "+mirror_root);
+os.makedirs(mirror_root)
+saveip = mirror_root + '/' + mirror_root + 'ips.txt'
+savegets = mirror_root + '/' + mirror_root + 'gets.txt'
+saveserverips = mirror_root + '/' + mirror_root + 'serverips.txt'
+shutil.copy('ips.txt', saveip)
+shutil.copy('tempgets.txt', savegets)
+shutil.copy('serverips.txt', saveserverips)
---
+++ web_mirror_replay_quic.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+from mininet.cli import CLI
+from mininet.log import lg
+from mininet.node import Node
+from mininet.topolib import TreeNet
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Host
+from mininet.link import Link
+
+import os
+import sys
+import shutil
+if (len(sys.argv) == 0):
+  ifile = 'serverips.txt'
+else:
+  folder = str(sys.argv[1])
+  ifile = folder + '/' + folder + 'serverips.txt'
+ip_addr=[]
+for f in open(ifile):
+  ip_addr.append(f.strip())
+
+current_working_dir=sys.argv[2]
+site_to_fetch=sys.argv[3]
+phantomjs_root=sys.argv[4]
+
+#################################
+class ProtoTester(Topo):
+    def __init__(self):
+        # Initialise topology
+        Topo.__init__(self)
+
+        # Create switch
+        s1 = self.addSwitch('s1')
+
+        # IP for client
+        client = self.addHost('client', ip='10.0.1.1', mac='00:00:00:00:00:01')
+        self.addLink(client, s1)
+
+        # IPs for sharded servers
+        server_list = []
+        for i in range(0,len(ip_addr)):
+          if i < 8:
+            server_list.append(self.addHost('server'+str(i), ip=ip_addr[i], mac='00:00:00:00:00:0'+str(i+2)))
+          if i > 7:
+            server_list.append(self.addHost('server'+str(i), ip=ip_addr[i], mac='00:00:00:00:00:'+str(i+2)))
+
+          # Add links
+          self.addLink(s1, server_list[i])
+
+if __name__ == '__main__':
+    lg.setLogLevel( 'info')
+    # Configure and start NATted connectivity
+    os.system( "killall -q controller" )
+    os.system( "killall -q phantomjs" )
+    os.system( "/etc/init.d/apache2 stop" )
+    os.system( "killall -s9 apache2" )
+    os.system( "killall -s9 quic_server" )
+    os.system("rm -rf /tmp/myprofdir/")
+    os.system("mkdir /tmp/myprofdir")
+    topo = ProtoTester()
+    net = Mininet(topo=topo, host=Host, link=Link)
+    net.start()
+
+    # Set /32 netmask and route for client
+    client = net.getNodeByName('client')
+    client.sendCmd('ifconfig client-eth0 10.0.1.1 netmask 0.0.0.0')
+    client.waitOutput()
+    client.sendCmd('route add default dev client-eth0')
+    client.waitOutput()
+    
+    # Set /32 netmask and route for all servers
+    #make folder for quic server to use (localhost:80)
+    if os.path.exists('/home/skype-alpha/July3rdGoogleMeeting/src/out/Release/localhost'):       
+        shutil.rmtree('/home/skype-alpha/July3rdGoogleMeeting/src/out/Release/localhost')
+    os.makedirs('/home/skype-alpha/July3rdGoogleMeeting/src/out/Release/localhost')
+    os.system( 'cp -R /home/skype-alpha/July3rdGoogleMeeting/src/out/Release/' + folder + ' /home/skype-alpha/July3rdGoogleMeeting/src/out/Release/localhost/' +folder)
+
+    server_names = ['']*len(ip_addr)
+    for i in range(0,len(ip_addr)):
+      server_names[i] = net.getNodeByName('server'+str(i))
+      server_names[i].sendCmd('ifconfig server'+str(i)+'-eth0 '+ip_addr[i]+' netmask 0.0.0.0');
+      server_names[i].waitOutput()
+      server_names[i].sendCmd('route add default dev server'+str(i)+'-eth0')   
+      server_names[i].waitOutput()
+      server_names[i].cmdPrint('nohup ' + '/home/skype-alpha/July3rdGoogleMeeting/src/out/Release/quic_server --quic_in_memory_cache_dir=/home/skype-alpha/July3rdGoogleMeeting/src/out/Release/localhost:93 --port=93' + ' --ip='+ip_addr[i]+' &')
+      server_names[i].waitOutput()
+
+
+    print "*** Hosts are running and can talk to each other"
+    print "*** Type 'exit' or control-D to shut down network"
+    #client.cmdPrint(phantomjs_root+'/bin/phantomjs '+phantomjs_root+'/examples/loadspeed.js '+site_to_fetch);
+    #client.waitOutput()
+    #client.sendCmd('su skype-alpha')
+    #client.waitOutput()
+    #client.cmdPrint('su skype-alpha')
+    #client.waitOutput()
+    #client.cmdPrint('python /home/skype-alpha/browser_automation/load_google.py')
+    #client.waitOutput()
+    CLI( net )
+    os.system( "cp /etc/hosts_original /etc/hosts" )
+    for u in range(0, len(ip_addr)):
+      os.system( "rm /etc/apache2/apache2" + str(u) + ".conf")
